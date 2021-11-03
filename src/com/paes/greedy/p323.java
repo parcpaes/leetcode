770b167abc1b44f03698671339768ed17d2e8c5d
parcpaes <spaesmx@gmail.com>
2021-11-02 20:11:25 -0400

    refactorized p323 problem

--- src/com/paes/greedy/p323.java
+++ src/com/paes/greedy/p323.java
@@ -5,14 +5,13 @@ import java.util.Collection;
 
 public class p323 {
     public static int maximum69Number(int num) {
-        int ones = (int) Math.pow(10,(int)Math.log10(num)+1)/9;
+        int len = (int)Math.log10(num);
+        int ones = (int) Math.pow(10,len+1)/9;
         if((num/9)==ones) return num;
         String value = String.valueOf(num);
-        char numbers[] = value.toCharArray();
-        numbers[value.indexOf('6')]='9';
-        return Integer.parseInt(String.valueOf(numbers));
+        return ((int) Math.pow(10,len-value.indexOf('6'))*3)+num;
     }
     public static void main(String[] args) {
-        System.out.println(maximum69Number(9669));
+        System.out.println(maximum69Number(6966));
     }
 }
